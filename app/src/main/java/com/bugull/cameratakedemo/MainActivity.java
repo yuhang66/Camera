2e97dad9b3e4ad13e10ee9103d2c20923cf69716
yuhang66 <873798964@qq.com>
2019-05-30 11:40:30 +0800

    调用系统相机拍照并显示图片以及从相册中选择图片展示

---
+++ app/src/main/java/com/bugull/cameratakedemo/MainActivity.java
@@ -0,0 +1,262 @@
+package com.bugull.cameratakedemo;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int REQUEST_PICK_IMAGE = 1; //相册选取
+    private static final int REQUEST_CAPTURE = 2;  //拍照
+    private static final int REQUEST_PICTURE_CUT = 3;  //剪裁图片
+    private static final int REQUEST_PERMISSION = 4;  //权限请求
+    private ImageView iv;
+
+    private String connec = "24.4e83c7fc343eeb9063273b998b8fa147.2592000.1561259470.282335-16323186";
+    private Button take_btn, album_btn;
+    private PermissionsChecker mPermissionsChecker; // 权限检测器
+    static final String[] PERMISSIONS = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA};
+    private Uri imageUri;//原图保存地址
+    private boolean isClickCamera;
+    private String imagePath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        initListener();
+        init();
+    }
+
+    private void initViews() {
+        take_btn = (Button) findViewById(R.id.take_btn);
+        album_btn = (Button) findViewById(R.id.album_btn);
+        iv = (ImageView) findViewById(R.id.iv);
+    }
+
+    private void initListener() {
+        take_btn.setOnClickListener(this);
+        album_btn.setOnClickListener(this);
+    }
+
+    private void init() {
+        mPermissionsChecker = new PermissionsChecker(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.take_btn:         //拍照咯
+                //检查权限(6.0以上做权限判断)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (mPermissionsChecker.lacksPermissions(PERMISSIONS)) {
+                        startPermissionsActivity();
+                    } else {
+                        openCamera();
+                    }
+                } else {
+                    openCamera();
+                }
+                isClickCamera = false;
+                break;
+            case R.id.album_btn:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (mPermissionsChecker.lacksPermissions(PERMISSIONS)) {
+                        startPermissionsActivity();
+                    } else {
+                        selectFromAlbum();
+                    }
+                } else {
+                    selectFromAlbum();
+                }
+                isClickCamera = true;
+                break;
+        }
+    }
+
+    /**
+     * 打开系统相机
+     */
+    private void openCamera() {
+        File file = new FileStorage().createIconFile();
+        Log.i("imageUri4",""+file);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+
+            imageUri = FileProvider.getUriForFile(MainActivity.this, "com.bugull.cameratakedemo.fileprovider", file);//通过FileProvider创建一个content类型的Uri
+            Log.i("imageUri1",""+imageUri);
+        } else {
+            imageUri = Uri.fromFile(file);
+            Log.i("imageUri2",""+imageUri);
+
+        }
+        Intent intent = new Intent();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); //添加这一句表示对目标应用临时授权该Uri所代表的文件
+        }
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);//设置Action为拍照
+        intent.putExtra("android.intent.extras.CAMERA_FACING", 1);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);//将拍取的照片保存到指定URI
+
+        Log.i("imageUri3",""+imageUri);
+        startActivityForResult(intent, REQUEST_CAPTURE);
+    }
+
+    /**
+     * 从相册选择
+     */
+    private void selectFromAlbum() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intent, REQUEST_PICK_IMAGE);
+    }
+
+    /**
+     * 裁剪
+     */
+    private void cropPhoto() {
+        File file = new FileStorage().createCropFile();
+        Uri outputUri = Uri.fromFile(file);//缩略图保存地址
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+        intent.setDataAndType(imageUri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("scale", true);
+        intent.putExtra("return-data", false);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true);
+        startActivityForResult(intent, REQUEST_PICTURE_CUT);
+    }
+
+    private void startPermissionsActivity() {//权限
+        PermissionsActivity.startActivityForResult(this, REQUEST_PERMISSION,
+                PERMISSIONS);
+    }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        imagePath = null;
+        imageUri = data.getData();
+        if (DocumentsContract.isDocumentUri(this, imageUri)) {
+            //如果是document类型的uri,则通过document id处理
+            String docId = DocumentsContract.getDocumentId(imageUri);
+            if ("com.android.providers.media.documents".equals(imageUri.getAuthority())) {
+                String id = docId.split(":")[1];//解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.downloads.documents".equals(imageUri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(imageUri.getScheme())) {
+            //如果是content类型的Uri，则使用普通方式处理
+            imagePath = getImagePath(imageUri, null);
+        } else if ("file".equalsIgnoreCase(imageUri.getScheme())) {
+            //如果是file类型的Uri,直接获取图片路径即可
+            imagePath = imageUri.getPath();
+        }
+
+        cropPhoto();
+    }
+
+    private void handleImageBeforeKitKat(Intent intent) {
+        imageUri = intent.getData();
+        imagePath = getImagePath(imageUri, null);
+        cropPhoto();
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        //通过Uri和selection老获取真实的图片路径
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_PICK_IMAGE://从相册选择
+                if (Build.VERSION.SDK_INT >= 19) {
+                    handleImageOnKitKat(data);
+                } else {
+                    handleImageBeforeKitKat(data);
+                }
+                break;
+            case REQUEST_CAPTURE://拍照
+                if (resultCode == RESULT_OK) {
+
+                    cropPhoto();
+                    Log.i("imageUri10",""+imageUri);
+
+
+
+
+
+                }
+                break;
+            case REQUEST_PICTURE_CUT://裁剪完成
+                Bitmap bitmap = null;
+                try {
+                    if (isClickCamera) {
+
+                        bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                    } else {
+                        bitmap = BitmapFactory.decodeFile(imagePath);
+                    }
+                    iv.setImageBitmap(bitmap);
+                    Log.i("imageUri11",""+bitmap);
+                } catch (Exception e) {
+                   e.printStackTrace();
+                }
+                break;
+            case REQUEST_PERMISSION://权限请求
+                if (resultCode == PermissionsActivity.PERMISSIONS_DENIED) {
+                    finish();
+                } else {
+                    if (isClickCamera) {
+                        openCamera();
+                    } else {
+                        selectFromAlbum();
+                    }
+                }
+                break;
+        }
+    }
+
+
+}
